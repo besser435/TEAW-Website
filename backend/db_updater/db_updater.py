25a8178c45df397e2ae21bfa6e37d555f669c334
Brandon <brandonesser435@gmail.com>
2024-11-28 16:26:09 -0700

    Add towns, nations tables, add UUID and name keys to players table

--- backend/db_updater/db_updater.py
+++ backend/db_updater/db_updater.py
@@ -15,13 +15,13 @@ ERROR_LOG = "errors.log"
 
 
 def update_players_table() -> None:
-    print("Updating players table")
-    start_time = time.time()
+    print("Updating players table...")
 
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
-
     response = requests.get(TAPI_URL + "/online_players")
+
+    start_time = time.time()
     if response.status_code == 200:
         data = response.json()
         online_players = data.get("online_players", {})
@@ -32,7 +32,9 @@ def update_players_table() -> None:
             afk_duration = player_data.get("afk_duration", 0)
             balance = player_data.get("balance", 0.0)
             title = player_data.get("title")
+            town = player_data.get("town")
             town_name = player_data.get("town_name")
+            nation = player_data.get("nation")
             nation_name = player_data.get("nation_name")
             last_online = int(time.time() * 1000)   # convert to ms, as that is what we do everywhere
 
@@ -40,19 +42,21 @@ def update_players_table() -> None:
             cursor.execute("""
                 INSERT INTO players (
                     uuid, name, online_duration, afk_duration, balance, 
-                    title, town_name, nation_name, last_online
+                    title, town, town_name, nation, nation_name, last_online
                 ) VALUES (
-                    ?, ?, ?, ?, ?, ?, ?, ?, ?
-                ) ON CONFLICT(uuid) DO UPDATE SET   -- Upsert if the player already exists
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?
+                ) ON CONFLICT(uuid) DO UPDATE SET
                     name = excluded.name,
                     online_duration = excluded.online_duration,
                     afk_duration = excluded.afk_duration,
                     balance = excluded.balance,
                     title = excluded.title,
+                    town = excluded.town,
                     town_name = excluded.town_name,
+                    nation = excluded.nation,
                     nation_name = excluded.nation_name,
                     last_online = excluded.last_online
-            """, (uuid, name, online_duration, afk_duration, balance, title, town_name, nation_name, last_online))
+            """, (uuid, name, online_duration, afk_duration, balance, title, town, town_name, nation, nation_name, last_online))
 
         conn.commit()
     else:
@@ -60,21 +64,21 @@ def update_players_table() -> None:
     conn.close()
 
     end_time = time.time()
-    print(f"Players table updated in {round((end_time - start_time) * 1000, 3)}ms\n")
+    print(f"Players table updated in {round((end_time - start_time) * 1000, 3)}ms\n")   # Does not include network request time
 
 
 def update_chat_table() -> None:
-    print("Updating chat table")
-    start_time = time.time()
+    print("Updating chat table...")
 
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
+    response = requests.get(TAPI_URL + "/chat_history")
 
+    start_time = time.time()
+    
     cursor.execute("SELECT MAX(timestamp) FROM chat")
     last_timestamp = cursor.fetchone()[0] or 0  # Default to 0 if no messages exist
 
-
-    response = requests.get(TAPI_URL + "/chat_history")
     if response.status_code == 200:
         chat_data = response.json()
 
@@ -100,8 +104,118 @@ def update_chat_table() -> None:
     conn.close()
 
     end_time = time.time()
-    print(f"Chat table updated in {round((end_time - start_time) * 1000, 3)}ms\n")
+    print(f"Chat table updated in {round((end_time - start_time) * 1000, 3)}ms\n")   # Does not include network request time
 
+
+def update_towns_table() -> None:
+    print("Updating towns table...")
+
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    response = requests.get(TAPI_URL + "/towny")
+
+    start_time = time.time()
+    if response.status_code == 200:
+        data = response.json()
+        towns = data.get("towns", {})
+
+        for town_uuid, town_data in towns.items():    # does not include residents array from TAPI
+            resident_tax_percent = town_data.get("resident_tax_percent", 0.0)
+            is_active = town_data.get("is_active", False)
+            balance = town_data.get("balance", 0.0)
+            nation = town_data.get("nation")
+            nation_name = town_data.get("nation_name")
+            mayor = town_data.get("mayor")
+            founder = town_data.get("founder")
+            name = town_data.get("name")
+            founded = town_data.get("founded")
+            claimed_chunks = town_data.get("claimed_chunks", 0)
+            tag = town_data.get("tag")
+            board = town_data.get("board")
+
+
+            cursor.execute("""
+                INSERT INTO towns (
+                    uuid, name, mayor, founder, balance, nation, nation_name, founded, resident_tax_percent, 
+                    is_active, claimed_chunks, tag, board
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT(uuid) DO UPDATE SET
+                    name=excluded.name,
+                    mayor=excluded.mayor,
+                    founder=excluded.founder,
+                    balance=excluded.balance,
+                    nation=excluded.nation,
+                    founded=excluded.founded,
+                    resident_tax_percent=excluded.resident_tax_percent,
+                    is_active=excluded.is_active,
+                    claimed_chunks=excluded.claimed_chunks,
+                    tag=excluded.tag,
+                    board=excluded.board
+            """, (town_uuid, name, mayor, founder, balance, nation, nation_name, founded, resident_tax_percent, 
+                is_active, claimed_chunks, tag, board))
+
+        conn.commit()
+    else:
+        print(f"Failed to fetch town data: {response.status_code}")
+    conn.close()
+
+    end_time = time.time()
+    print(f"Towns table updated in {round((end_time - start_time) * 1000, 3)}ms\n")   # Does not include network request time
+
+
+def update_nations_table() -> None:
+    print("Updating nations table...")
+
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    response = requests.get(TAPI_URL + "/towny")
+
+    start_time = time.time()
+    if response.status_code == 200:
+        data = response.json()
+        nations = data.get("nations", {})
+
+        for nation_id, nation_data in nations.items():
+            leader = nation_data.get("leader")
+            capitol_town = nation_data.get("capitol_town")
+            capitol_town_name = nation_data.get("capitol_town_name")
+            balance = nation_data.get("balance", 0.0)
+            town_tax_dollars = nation_data.get("town_tax_dollars", 0.0)
+            name = nation_data.get("name")
+            founded = nation_data.get("founded")
+            tag = nation_data.get("tag")
+            board = nation_data.get("board")
+
+            cursor.execute("""
+                INSERT INTO nations (
+                    uuid, name, leader, capitol_town, capitol_town_name, balance, 
+                    town_tax_dollars, founded, tag, board
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT(uuid) DO UPDATE SET
+                    name=excluded.name,
+                    leader=excluded.leader,
+                    capitol_town=excluded.capitol_town,
+                    capitol_town_name=excluded.capitol_town_name,
+                    balance=excluded.balance,
+                    town_tax_dollars=excluded.town_tax_dollars,
+                    founded=excluded.founded,
+                    tag=excluded.tag,
+                    board=excluded.board
+            """, (
+                nation_id, name, leader, capitol_town, capitol_town_name, balance, 
+                town_tax_dollars, founded, tag, board
+            ))
+
+        conn.commit()
+    else:
+        print(f"Failed to fetch nation data: {response.status_code}")
+    
+    conn.close()
+
+    end_time = time.time()
+    print(f"Nations table updated in {round((end_time - start_time) * 1000, 3)}ms\n")   # Does not include network request time
 
 
 def log_error(error: str) -> None:
@@ -112,16 +226,24 @@ def log_error(error: str) -> None:
     print(error_string)
 
 
+
 if __name__ == "__main__":
     try:
         while True: 
-            # TODO: should be async, so we can have different intervals for different tasks.
-            # chat should be updated frequently, but towns only needs to be ran every few minutes.
+            """TODO: 
+            Should be async, so we can have different intervals for different tasks.
+            chat should be updated frequently, but towns only needs to be ran every few minutes.
+
+            Should raise an error if an update takes longer than a few hundred milliseconds
+            """
+
             update_players_table()
             update_chat_table()
+            update_towns_table()
+            update_nations_table()
             
 
-            time.sleep(5)
+            time.sleep(1)
     except Exception:
         log_error(traceback.format_exc())
 
--- backend/db_updater/db_utils.py
+++ backend/db_updater/db_utils.py
@@ -21,12 +21,13 @@ def create_tables(db_file=DB_FILE):
             afk_duration INTEGER,
             balance REAL,
             title TEXT,
-            town_name TEXT,                
+            town TEXT,           
+            town_name TEXT, 
+            nation TEXT,    
             nation_name TEXT,              
             last_online INTEGER     -- added by db_updater.py, not TAPI
         )
     """)
-
 
     # Create chat table
     cursor.execute("""
@@ -39,18 +40,41 @@ def create_tables(db_file=DB_FILE):
         )
     """)
 
+    # Create towns table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS towns (
+            uuid TEXT PRIMARY KEY,
+            name TEXT NOT NULL,
+            mayor TEXT NOT NULL,
+            founder TEXT NOT NULL,
+            balance REAL NOT NULL,
+            nation TEXT,
+            nation_name TEXT,
+            founded INTEGER NOT NULL,
+            resident_tax_percent REAL NOT NULL,
+            is_active BOOLEAN NOT NULL,
+            claimed_chunks INTEGER NOT NULL,
+            tag TEXT,
+            board TEXT
+        )
+    """)
 
-    # # Create towns table
-    # cursor.execute("""
-    #     CREATE TABLE IF NOT EXISTS towns (
-                   
-    # """)
+    # Create nations table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS nations (
+            uuid TEXT PRIMARY KEY,
+            name TEXT NOT NULL,
+            leader TEXT NOT NULL,
+            capitol_town TEXT NOT NULL,
+            capitol_town_name TEXT,
+            balance REAL NOT NULL,
+            town_tax_dollars REAL NOT NULL,
+            founded INTEGER NOT NULL,
+            tag TEXT,
+            board TEXT
+        )
+    """)
 
-    # # Create nations table
-    # cursor.execute("""
-    #     CREATE TABLE IF NOT EXISTS nations (
-                   
-    # """)
 
 
 
@@ -73,6 +97,7 @@ def drop(db_file=DB_FILE, table=None):
 # do lookups on the chat table to prevent adding duplicates.
 # This will move the chat messages to an archive table, and delete the messages in the chat table.
 def archive_chat_table(db_file=DB_FILE):
+    # Keep the 1000 most recent messages in the chat table, so that they can still be displayed in the frontend
     pass
 
 
@@ -80,5 +105,7 @@ def archive_chat_table(db_file=DB_FILE):
 if __name__ == "__main__":
     drop(DB_FILE, "players")
     drop(DB_FILE, "chat")
+    drop(DB_FILE, "towns")
+    drop(DB_FILE, "nations")
     create_tables()
 
