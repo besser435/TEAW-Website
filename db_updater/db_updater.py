976d483e043d8014e74ed28d64822a5307f445f5
Brandon <brandonesser435@gmail.com>
2024-12-16 00:40:14 -0700

    Add UUID to chat endpoint, improve chat API

--- db_updater/db_updater.py
+++ db_updater/db_updater.py
@@ -130,6 +130,7 @@ def update_chat_table() -> None:
 
             for chat_entry in chat_data:
                 sender = chat_entry.get("sender")
+                sender_uuid = chat_entry.get("sender_uuid")
                 message = chat_entry.get("message")
                 timestamp = chat_entry.get("timestamp")
                 message_type = chat_entry.get("type")
@@ -140,9 +141,9 @@ def update_chat_table() -> None:
                     # Since we rely on milliseconds and not some other key, we need to ensure TAPI has 
                     # precision to the millisecond so the same message isn't inserted multiple times.
                     cursor.execute("""
-                        INSERT INTO chat (sender, message, timestamp, type)
-                        VALUES (?, ?, ?, ?)
-                    """, (sender, message, timestamp, message_type))
+                        INSERT INTO chat (sender, sender_uuid, message, timestamp, type)
+                        VALUES (?, ?, ?, ?, ?)
+                    """, (sender, sender_uuid, message, timestamp, message_type))
 
             conn.commit()
             upsert_variable("last_chat_update", int(time.time() * 1000))
@@ -369,7 +370,7 @@ if __name__ == "__main__":
 
             print(f"Updated info at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}. Total time taken was {round((end_time - start_time) * 1000, 2)}ms")
 
-            time.sleep(5)
+            time.sleep(2)
     except requests.exceptions.ConnectTimeout as e:
         # When TEAW restarts, it can rarely cause requests to not be able to reconnect.
         # This should restart the script and fix the issue, hopefully.
--- web/api_routes.py
+++ web/api_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify, send_from_directory
+from flask import Blueprint, jsonify, send_from_directory, request
 from werkzeug.exceptions import NotFound
 import sqlite3
-import os
+import traceback
 import time
 
 
@@ -10,11 +10,14 @@ from config import TEAW_DB_FILE, STATS_DB_FILE, PLAYER_BODY_SKIN_DIR, PLAYER_FAC
 
 api_routes = Blueprint("api_blueprint", __name__)
 
+
+# NOTE: Routes only return the required data for each page, not every column in the database.
+
 @api_routes.route("/api")
 def home():
     return "ok", 200
 
-# TODO
+
 @api_routes.route("/api/status")    # For status indicator in the navbar. Call every 2s
 def get_status():
     try:
@@ -49,14 +52,15 @@ def get_status():
 
         return dict(status=status, online_players=online_players_count), 200
     except Exception as e:
-        log.error(f"Internal error getting `status`: {e}")
+        log.error(f"Internal error getting `status`: {traceback.format_exc()}")
         return "internal error", 500
 
 
-@api_routes.route("/api/players")   # For the players page. Only returns the required data, not every column
+@api_routes.route("/api/players")
 def get_all_players():
     try:
         with sqlite3.connect(TEAW_DB_FILE) as conn:
+            conn.row_factory = sqlite3.Row
             cursor = conn.cursor()
 
             cursor.execute("""
@@ -76,48 +80,68 @@ def get_all_players():
                 ORDER BY status, name
             """)
             
-            players = [
-                {
-                    'uuid': row[0],
-                    'name': row[1],
-                    'online_duration': row[2],
-                    'afk_duration': row[3],
-                    'town_name': row[4],
-                    'nation_name': row[5],
-                    'status': row[6]
-                }
-                for row in cursor.fetchall()
-            ]
+            players = [dict(row) for row in cursor.fetchall()]
 
         return jsonify(players), 200
     except Exception as e:
-        log.error(f"Internal error getting `players`: {e}")
+        log.error(f"Internal error getting `players`: {traceback.format_exc()}")
         return "internal error", 500
 
 
-@api_routes.route("/api/towns")     # For the towns page. Only returns the required data, not every column
+@api_routes.route("/api/towns")
 def get_all_towns():
     try:
         return "ok", 200
     except Exception as e:
-        log.error(f"Internal error getting `towns`: {e}")
+        log.error(f"Internal error getting `towns`: {traceback.format_exc()}")
         return "internal error", 500
 
 
 
-@api_routes.route("/api/chat")
-def get_chat():
+@api_routes.route("/api/chat_messages")
+def get_chat_messages():
     try:
-        return "ok", 200
+        oldest_message_id = request.args.get("oldest_message_id", type=int)
+        newest_message_id = request.args.get("newest_message_id", type=int)
+        
+        with sqlite3.connect(TEAW_DB_FILE) as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            
+            if oldest_message_id:   # Older messages before a certain ID (used when the user scrolls and wants older messages)
+                cursor.execute("""
+                    SELECT id, sender, sender_uuid, message, timestamp, type
+                    FROM chat
+                    WHERE id < ?
+                    ORDER BY id DESC
+                    LIMIT 200
+                """, (oldest_message_id,))
+            
+            elif newest_message_id: # New messages after a certain ID (used for updates)
+                cursor.execute("""
+                    SELECT id, sender, sender_uuid, message, timestamp, type
+                    FROM chat
+                    WHERE id > ?
+                    ORDER BY id ASC
+                    LIMIT 200
+                """, (newest_message_id,))
+            
+            else:   # All of the newest messages (used on page load)
+                cursor.execute("""
+                    SELECT id, sender, sender_uuid, message, timestamp, type
+                    FROM chat
+                    ORDER BY id DESC
+                    LIMIT 200
+                """)
+
+            chat_messages = [dict(row) for row in cursor.fetchall()]
+
+        chat_messages.reverse() # TODO: might not be needed
+
+        return jsonify(chat_messages), 200
     except Exception as e:
-        log.error(f"Internal error getting `chat`: {e}")
+        log.error(f"Internal error getting `chat_messages`: {traceback.format_exc()}")
         return "internal error", 500
-
-
-
-
-
-
 
 
 
@@ -128,7 +152,7 @@ def get_player_skin(uuid):
     except NotFound:
         return "player skin not found", 404
     except Exception as e:
-        log.error(f"Internal error getting `player_skin`: {e}")
+        log.error(f"Internal error getting `player_skin`: {traceback.format_exc()}")
         return "internal error", 500
 
 
@@ -139,5 +163,5 @@ def get_player_face(uuid):
     except NotFound:
         return "player face not found", 404
     except Exception as e:
-        log.error(f"Internal error getting `player_face`: {e}")
+        log.error(f"Internal error getting `player_face`: {traceback.format_exc()}")
         return "internal error", 500
