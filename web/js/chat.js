91f342ff67334a205612b45f2af324b50f0e1b20
Brandon <brandonesser435@gmail.com>
2024-12-17 07:13:06 -0700

    Work towards completion on Chat page
    * Bolded the proper parts of the message depending on type
    * Periodically update timestamps after the message has been created
    *Change time bubble to use the current phase instead of the exact time
    * Make message info height match the message div

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,6 +1,3 @@
-const updateRate = 2_000;
-
-
 // Scroll detection listener for when the user scrolls up and wants to view older messages
 // NOTE: untested. probably doesnt work given we dont set a scroll threshold.
 // document.getElementById("chat-feed").addEventListener("scroll", function() {
@@ -16,13 +13,16 @@ const updateRate = 2_000;
 // });
 
 
+const updateRate = 2_000;
+
+
 // Should only be called for player messages. Otherwise use CSS to select the correct icon
 function getPlayerProfilePicObj(sender_uuid) {
     // For if we ever add Discord sender PFPs
     // if (messageType === "discord") {
     //     const discordProfilePic = document.createElement("img");
     //     discordProfilePic.className = "profile-pic";
-    //     discordProfilePic.src = "";   // Google Material Icon
+    //     discordProfilePic.src = "";
 
     //     return discordProfilePic;
 
@@ -32,6 +32,23 @@ function getPlayerProfilePicObj(sender_uuid) {
     profilePic.src = "/api/player_face/" + sender_uuid;
 
     return profilePic;
+}
+
+function messageBolder(message, messageType) {
+    switch (messageType) {
+        case "join":
+        case "quit":
+        case "death":
+        case "status":
+            return message.replace(/^(\w+)/, "<b>$1</b>");
+        
+        case "advancement":
+            return message
+                .replace(/^(\w+)/, "<b>$1</b>")
+                .replace(/(\[.*\])/, "<b>$1</b>");
+        default:
+            return message;
+    }
 }
 
 
@@ -65,7 +82,7 @@ class Message {
      * @param {string} sender - The name of the sender.
      * @param {string} sender_uuid - The UUID of the sender. May be a player or a Discord user.
      * @param {string} message - The content of the message.
-     * @param {number} timestamp - The timestamp of the message. Provided epoch will be formatted.
+     * @param {number} timestamp - The timestamp of the message.
      * @param {string} type - The type of the message.
      */
     constructor(id, sender, sender_uuid, message, timestamp, type) {
@@ -78,7 +95,8 @@ class Message {
         /** @type {string} */
         this.message = message;
         /** @type {string} */
-        this.timestamp = formatEpochTime(timestamp);
+        this.formatted_timestamp = formatEpochTime(timestamp);
+        this.epoch_timestamp = timestamp;
         /** @type {string} */
         this.type = type;
 
@@ -92,9 +110,8 @@ class Message {
     }
 }
 
-// NOTE: newest messages will appear at the top. fix later if desired
-// Need to add bold tags depending on message type
-function createMessage(messageObj) {
+
+function addMessage(messageObj) {
     const chatFeed = document.getElementsByClassName("chat-feed");  // Main message container
 
 
@@ -121,48 +138,43 @@ function createMessage(messageObj) {
     // Add timestamp
     let timestamp = document.createElement("div");
     timestamp.className = "timestamp";
-    timestamp.innerHTML = messageObj.timestamp;
+    timestamp.innerHTML = messageObj.formatted_timestamp;
     messageInfo.appendChild(timestamp);
+    timestamp.setAttribute("data-epoch-timestamp", messageObj.epoch_timestamp); // For updating timestamps later
 
     // Add message type CSS data class
     messageInfo.setAttribute("data-message-type", messageObj.type);
-
 
     // The actual message part
     // Message text
     const messageText = document.createElement("div");
     messageText.className = "message-text";
-    messageText.innerHTML = messageObj.message;
+    //messageText.innerHTML = messageObj.message;
+    messageText.innerHTML = messageBolder(messageObj.message, messageObj.type);
 
 
     // Package up the message info and message text
     const messageContainer = document.createElement("div");
     messageContainer.className = "message-container";
     messageContainer.id = messageObj.id;
+    messageContainer.style.display = "flex"; // So we can hide messages on searches later
 
     messageContainer.appendChild(messageInfo);
     messageContainer.appendChild(messageText);
 
 
+
     // NOTE: newest messages will appear at the top. fix later if desired
     chatFeed[0].appendChild(messageContainer);
-    // while debugging
-    messageContainer.style.color = "white";
-    chatFeed[0].appendChild(document.createElement("br"));
-    //chatFeed.scrollTop = messageWindow.scrollHeight;
 }
 
 
 // TODO: handle errors
 let firstLoad = true;
-function updateNewMessages(oldest_message_id = 0) {
-    // If its the first load, request messages with no filter to get the newest messages.
-    // Otherwise, send a request for messages with an ID greater than the newest message ID.
-
-
+function getNewMessages(oldest_message_id = 0) {
     const processMessages = (messages) => {
         for (const message of messages) {
-            createMessage(new Message(
+            addMessage(new Message(
                 message.id, 
                 message.sender, 
                 message.sender_uuid, 
@@ -172,7 +184,6 @@ function updateNewMessages(oldest_message_id = 0) {
             ));
         }
     };
-
 
     if (firstLoad) {    // First load, get all messages (200 newest messages)
         fetch("/api/chat_messages")
@@ -199,16 +210,28 @@ function updateNewMessages(oldest_message_id = 0) {
             });
     }
 }
-updateNewMessages();
-setInterval(updateNewMessages, updateRate);
+getNewMessages();
+setInterval(getNewMessages, updateRate);
+
+
+function updateMessageTimestamps() {
+    // Once messages are added, their timestamps are not magically updated.
+    // This fixes that.
+
+    const formatted_timestamps = document.getElementsByClassName("timestamp");
+    for (const timestamp of formatted_timestamps) {
+        const epoch_timestamp = parseInt(timestamp.getAttribute("data-epoch-timestamp"));
+        timestamp.innerHTML = formatEpochTime(epoch_timestamp);
+    }
+}
+setInterval(updateMessageTimestamps, 30_000);
 
 
 function updateInfoBubbles() {
     const messagesLoggedBubble = document.getElementById("message-count");
     const daysElapsedBubble = document.getElementById("days-elapsed");
     const worldWeatherBubble = document.getElementById("world-weather");
-
-    const worldTimeBubble = document.getElementById("world-time");  // Either remove, or add code to interpolate time between updates
+    const worldTimeBubble = document.getElementById("world-time");
 
     fetch("/api/chat_misc")
         .then(response => response.json())
@@ -217,19 +240,56 @@ function updateInfoBubbles() {
             daysElapsedBubble.innerHTML = data.days_elapsed.toLocaleString();
             worldWeatherBubble.innerHTML = data.world_weather;
 
-            // https://minecraft.wiki/w/Daylight_cycle
-            // Replace real time with time according to the Minecraft day cycle. This prevents
-            // sporadic time updates.
-            worldTimeBubble.innerHTML = data.world_time;
+            // We use stages rather than the real time, as the time is only updated every few seconds,
+            // and would look funny if it was constantly changing by large amounts.
+            const hour = parseInt(data.world_time.split(":")[0]);
+            let timeStage;
+
+            // Stages according to: https://minecraft.wiki/w/Daylight_cycle#24-hour_Minecraft_day
+            if (hour >= 6 && hour < 12) {
+                timeStage = "Day";
+            } else if (hour >= 12 && hour < 18) {
+                timeStage = "Noon";
+            } else if (hour >= 18 && hour < 19) {
+                timeStage = "Sunset";
+            } else if (hour >= 19 || hour < 5) {
+                timeStage = "Night";
+            } else if (hour >= 0 && hour < 5) {
+                timeStage = "Midnight";
+            } else if (hour >= 5 && hour < 6) {
+                timeStage = "Sunrise";
+            } else {
+                timeStage = "Unknown";
+            }
+
+            worldTimeBubble.innerHTML = timeStage;
         });
 }
 updateInfoBubbles();
 setInterval(updateInfoBubbles, updateRate);
 
 
+// Style stuff
+function setMessageInfoHeight() {
+    const messageContainers = document.querySelectorAll(".message-container");
+
+    messageContainers.forEach(container => {
+        const messageInfo = container.querySelector(".message-info");
+        if (messageInfo) {
+            messageInfo.style.height = "auto";
+
+            const containerHeight = container.offsetHeight;
+
+            messageInfo.style.height = `${containerHeight}px`;
+        }
+    });
+}
+window.addEventListener("load", setMessageInfoHeight);
+window.addEventListener("resize", setMessageInfoHeight);
+
 
 // status
-// createMessage(new Message(
+// addMessage(new Message(
 //     1, 
 //     "SERVER", 
 //     "null", 
@@ -239,7 +299,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // join
-// createMessage(new Message(
+// addMessage(new Message(
 //     2, 
 //     "SERVER", 
 //     "5663c72f-18c5-4012-b28c-78784c2ca736", 
@@ -249,7 +309,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // chat
-// createMessage(new Message(
+// addMessage(new Message(
 //     3, 
 //     "SaxboyLaFranks", 
 //     "6c7ab286-3ea3-42b4-af47-55376c963d92", 
@@ -259,7 +319,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // discord
-// createMessage(new Message(
+// addMessage(new Message(
 //     4, 
 //     "besser", 
 //     "232014294303113216", 
@@ -269,7 +329,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // advancement
-// createMessage(new Message(
+// addMessage(new Message(
 //     5, 
 //     "SERVER", 
 //     "5663c72f-18c5-4012-b28c-78784c2ca736", 
@@ -279,7 +339,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // death
-// createMessage(new Message(
+// addMessage(new Message(
 //     6, 
 //     "SERVER", 
 //     "5663c72f-18c5-4012-b28c-78784c2ca736", 
@@ -289,7 +349,7 @@ setInterval(updateInfoBubbles, updateRate);
 // ));
 
 // // quit
-// createMessage(new Message(
+// addMessage(new Message(
 //     7, 
 //     "SERVER", 
 //     "5663c72f-18c5-4012-b28c-78784c2ca736", 
@@ -298,8 +358,8 @@ setInterval(updateInfoBubbles, updateRate);
 //     "quit"
 // ));
 
-// // tests
-// createMessage(new Message(
+// tests
+// addMessage(new Message(
 //     8, 
 //     "SaxboyLaFranks", 
 //     "6c7ab286-3ea3-42b4-af47-55376c963d92", 
@@ -308,7 +368,7 @@ setInterval(updateInfoBubbles, updateRate);
 //     "chat"
 // ));
 
-// createMessage(new Message(
+// addMessage(new Message(
 //     9, 
 //     "SaxboyLaFranks", 
 //     "6c7ab286-3ea3-42b4-af47-55376c963d92", 
@@ -317,12 +377,12 @@ setInterval(updateInfoBubbles, updateRate);
 //     "chat"
 // ));
 
-// createMessage(new Message(
+// addMessage(new Message(
 //     10, 
 //     "SaxboyLaFranks", 
 //     "6c7ab286-3ea3-42b4-af47-55376c963d92", 
 //     "control char rendering test \"real\" \"fake\"",  
-//     1734194977000, 
+//     Date.now() - 58_000, 
 //     "chat"
 // ));
 
